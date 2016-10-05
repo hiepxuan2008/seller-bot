ae83e1e8990480a6cd375234fa9e5156b95c0caf
mthit2012 <hiepmt@vng.com.vn>
2016-10-06 01:36:46 +0700

    update

--- app.py
+++ app.py
@@ -1,306 +1,196 @@
 import os
-import sys
 import json
+import log
 
-import requests
 from flask import Flask, request
+from messenger_api import *
 
 app = Flask(__name__)
 
-
 @app.route('/', methods=['GET'])
 def verify():
-	# when the endpoint is registered as a webhook, it must echo back
-	# the 'hub.challenge' value it receives in the query arguments
-	if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
-		if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
-			return "Verification token mismatch", 403
-		return request.args["hub.challenge"], 200
+    # when the endpoint is registered as a webhook, it must echo back
+    # the 'hub.challenge' value it receives in the query arguments
+    if request.args.get("hub.mode") == "subscribe" and request.args.get("hub.challenge"):
+        if not request.args.get("hub.verify_token") == os.environ["VERIFY_TOKEN"]:
+            return "Verification token mismatch", 403
+        return request.args["hub.challenge"], 200
 
-	return "Hello world", 200
+    return "Hello world", 200
 
 
 @app.route('/', methods=['POST'])
 def webhook():
 
-	# endpoint for processing incoming messaging events
+    # endpoint for processing incoming messaging events
 
-	data = request.get_json()
-	log(data)  # you may not want to log every incoming message in production, but it's good for testing
+    data = request.get_json()
+    log.log(data)  # you may not want to log every incoming message in production, but it's good for testing
 
-	if data["object"] == "page":
+    if data["object"] == "page":
 
-		for entry in data["entry"]:
-			for messaging_event in entry["messaging"]:
+        for entry in data["entry"]:
+            for messaging_event in entry["messaging"]:
 
-				if messaging_event.get("message"):  # someone sent us a message
-					sender_id = messaging_event["sender"]["id"]		# the facebook ID of the person sending you the message
-					recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-					message_text = messaging_event["message"]["text"]  # the message's text
+                # https://developers.facebook.com/docs/messenger-platform/webhook-reference/message-received
+                if messaging_event.get("message"):  # someone sent us a message
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    message = messaging_event["message"]  # the message
+                    on_message_received(sender_id, recipient_id, message)
 
-					onMessageEvent(sender_id, recipient_id, message_text)
+                if messaging_event.get("delivery"):  # delivery confirmation
+                    pass
 
-				if messaging_event.get("delivery"):  # delivery confirmation
-					pass
+                if messaging_event.get("optin"):  # optin confirmation
+                    pass
 
-				if messaging_event.get("optin"):  # optin confirmation
-					pass
+                # https://developers.facebook.com/docs/messenger-platform/webhook-reference/postback-received
+                if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
+                    sender_id = messaging_event["sender"]["id"]        # the facebook ID of the person sending you the message
+                    recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
+                    payload = messaging_event["postback"]["payload"]  # the payload text
+                    on_postback_received(sender_id, recipient_id, payload)
+    
+    return "ok", 200
 
-				if messaging_event.get("postback"):  # user clicked/tapped "postback" button in earlier message
-					sender_id = messaging_event["sender"]["id"]		# the facebook ID of the person sending you the message
-					recipient_id = messaging_event["recipient"]["id"]  # the recipient's ID, which should be your page's facebook ID
-					payload = messaging_event["postback"]["payload"]  # the payload text
-					onPostbackEvent(sender_id, recipient_id, payload)
+params = {"access_token": os.environ["PAGE_ACCESS_TOKEN"]}
+headers = {"Content-Type": "application/json"}
+messenger = MessengerApi(params, headers)
 
-	return "ok", 200
 
-def postData(data):
-	params = {
-		"access_token": os.environ["PAGE_ACCESS_TOKEN"]
-	}
-	headers = {
-		"Content-Type": "application/json"
-	}
+def on_message_received(sender_id, recipient_id, message):
+    messenger.do_sender_actions(sender_id)
+    if not message.get("text"):
+        return
 
-	r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=params, headers=headers, data=data)
-	if r.status_code != 200:
-		log(r.status_code)
-		log(r.text)
+    message_text = message.get("text")
+    if message_text == "hello":
+        help(sender_id)
 
-def onPostbackEvent(sender_id, recipient_id, payload):
-	if payload == "T_SHIRT":
-		showTShirtProducts(sender_id)
-	# elif payload == "FEATURE":
-	# 	doMoreFeature(sender_id)
-	# elif payload == "LOCATION":
-	# 	showLocation(sender_id)
-	# elif payload == "VIDEO":
-	# 	showVideo(sender_id)
+
+def on_postback_received(sender_id, recipient_id, payload):
+    if payload == "T_SHIRT":
+        showTShirtProducts(sender_id)
+    # elif payload == "FEATURE":
+    #      doMoreFeature(sender_id)
+    # elif payload == "LOCATION":
+    #     showLocation(sender_id)
+    # elif payload == "VIDEO":
+    #     showVideo(sender_id)
+
 
 def showTShirtProducts(recipient_id):
-	elements = [{
-					"title": "Welcome to Peter\'s Hats",
-					"item_url": "https://petersfancybrownhats.com",
-					"image_url": "https://petersfancybrownhats.com/company_image.png",
-					"subtitle": "We\'ve got the right hat for everyone.",
-					"buttons": [{
-						"type": "web_url",
-						"url": "https://petersfancybrownhats.com",
-						"title": "View Website"
-					}, {
-						"type": "postback",
-						"title": "Start Chatting",
-						"payload": "DEVELOPER_DEFINED_PAYLOAD"
-					}]
-				}]
-				
-	doGenericTemplate(recipient_id, elements)
+    elements = [{
+                    "title": "Nice Blue T-Shirt - $19.99",
+                    "item_url": "http://www.lazada.vn/ao-thun-nam-co-tru-xanh-navi-2035572.html",
+                    "image_url": "http://vn-live-02.slatic.net/p/ao-thun-nam-co-tru-xanh-co-vit-1405-3755302-0a0daa09d238345d6a267ba403f7abbe-catalog_233.jpg",
+                    "buttons":[
+                         {
+                            "type":"web_url",
+                            "url":"http://www.lazada.vn/ao-thun-nam-co-tru-xanh-navi-2035572.html",
+                            "title":"Show now"
+                         },
+                         {
+                             "type": "postback",
+                             "title":"More feature",
+                             "payload" : "FEATURE"
+                         }
+                    ]
+                },
+                {    
+                    "title": "Light Green T-Shirt - $21.99",
+                    "item_url": "http://zanado.com/ao-thun-nam-jackies-b202-dep-gia-re-sid48907.html?color=98",
+                    "image_url": "http://a4vn.com/media/catalog/product/cache/all/thumbnail/255x298/7b8fef0172c2eb72dd8fd366c999954c/1/3/13_40_2.jpg",
+                    "buttons":[
+                         {
+                            "type":"web_url",
+                            "url":"http://zanado.com/ao-thun-nam-jackies-b202-dep-gia-re-sid48907.html?color=98",
+                            "title":"Show now"
+                         }
+                    ]
+                },
+                {    
+                    "title": "Raglan T-Shirt red & white- $12.99",
+                    "item_url": "http://www.lazada.vn/ao-thun-nam-tay-raglan-do-do-phoi-trang-2056856.html?mp=1",
+                    "image_url": "http://vn-live-01.slatic.net/p/ao-thun-nam-tay-raglan-do-do-phoi-trang-2581-6586502-2d977472b068b70467eeb4e9d2e1122d-catalog_233.jpg",
+                    "buttons":[
+                         {
+                            "type":"web_url",
+                            "url":"http://www.lazada.vn/ao-thun-nam-tay-raglan-do-do-phoi-trang-2056856.html?mp=1",
+                            "title":"Show now"
+                         }
+                    ]
+                }]
 
-def onMessageEvent(sender_id, recipient_id, message_text):
-	doSenderActions(sender_id)
-	if message_text == "hello":
-		greeting(sender_id)
+    messenger.do_generic_template(recipient_id, elements)
 
-def greeting(sender_id):
-	text = "Welcome to Nova shop, what are you looking for today?"
-	buttons = [
-				  {
-					"type":"postback",
-					"title":"T-Shirt",
-					"payload":"T_SHIRT"
-				  },
-				  {
-					"type":"postback",
-					"title":"Jean",
-					"payload":"JEAN"
-				  },
-				  {
-					"type":"postback",
-					"title":"Wallet",
-					"payload":"WALLET"
-				  }
-			]
-	doButtonTemplate(sender_id, text, buttons)
+def doMoreFeature(recipient_id):
+    data = json.dumps({
+    "recipient":{
+        "id":recipient_id
+      },
+      "message":{
+        "attachment":{
+          "type":"template",
+          "payload":{
+            "template_type":"button",
+            "text":"What do you want to do next?",
+            "buttons":[
+              {
+                "type":"postback",
+                "title":"Products on Video",
+                "payload": "VIDEO"
+              },
+              {
+                "type":"postback",
+                "title":"Shop location",
+                "payload":"LOCATION"
+              },
+              {
+                "type":"postback",
+                "title":"Show webiste",
+                "payload":"WEBSITE"
+              }
+            ]
+          }
+        }
+      }
+    })
+    messenger.postData(data)
 
 
+def help(recipient_id):
+    text = "Hi, Can I help you?"
+    buttons = [
+        PostbackButton("Go Shopping", "GO_SHOPPING").to_json(),
+        PostbackButton("Shop Location", "SHOP_LOCATION").to_json(),
+        PostbackButton("Call For Help", "CALL_FOR_HELP").to_json()
+    ]
+    messenger.do_button_template(recipient_id, text, buttons)
 
-# -------------------------- FB Messenger API Functions -----------------
-# Sender Actions
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/sender-actions
-def doSenderActions(recipient_id):
-	data = json.dumps({
-	  "recipient":{
-	  	"id":recipient_id
-	  },
-	  "sender_action":"typing_on"
-	})
-	postData(data)
 
-# Text Message
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/text-message
-def doTextMessage(recipient_id, message_text):
-	data = json.dumps({
-		"recipient": {
-			"id": recipient_id
-		},
-		"message": {
-			"text": message_text
-		}
-	})
-	postData(data)
-
-# Image Attachment
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/image-attachment
-def doImageAttachment(recipient_id, image_url):
-	data = json.dumps({
-			"recipient":{
-				"id":recipient_id
-			},
-			"message":{
-				"attachment":{
-				"type":"image",
-				"payload":{
-					"url":image_url
-				}
-			}
-		}
-	})
-	postData(data)
-
-# Audio Attachment
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/audio-attachment
-def doAudioAttachment(recipient_id, audio_url):
-	data = json.dumps({
-			"recipient":{
-				"id":recipient_id
-			},
-			"message":{
-				"attachment":{
-				"type":"audio",
-				"payload":{
-					"url":audio_url
-				}
-			}
-		}
-	})
-	postData(data)
-
-# Video Attachment
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/video-attachment
-def doVideoAttachment(recipient_id, video_url):
-	data = json.dumps({
-			"recipient":{
-				"id":recipient_id
-			},
-			"message":{
-				"attachment":{
-				"type":"video",
-				"payload":{
-					"url":video_url
-				}
-			}
-		}
-	})
-	postData(data)
-
-# File Attachment
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/file-attachment
-def doFileAttachment(recipient_id, file_url):
-	data = json.dumps({
-			"recipient":{
-				"id":recipient_id
-			},
-			"message":{
-				"attachment":{
-				"type":"file",
-				"payload":{
-					"url":file_url
-				}
-			}
-		}
-	})
-	postData(data)
-
-# Generic Template
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/generic-template
-def doGenericTemplate(recipient_id, elements):
-	data = json.dumps({
-		"recipient":{
-			"id":recipient_id
-		},
-		"message":{
-			"attachment":{
-			"type":"template",
-			"payload":{
-				"template_type":"generic",
-				"elements":elements
-				}
-			}
-		}
-	})
-
-	postData(data)
-
-# Button Template
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/button-template
-# Params: 
-#	text: must be UTF-8 and has a 320 character limit
-#	buttons: is limited to 3
-
-def doButtonTemplate(recipient_id, text, buttons):
-	data = json.dumps({
-		  "recipient":{
-			"id":recipient_id
-		  },
-		  "message":{
-			"attachment":{
-			  "type":"template",
-			  "payload":{
-				"template_type":"button",
-				"text":text,
-				"buttons": buttons
-			  }
-			}
-		  }
-		})
-
-	postData(data)
-
-# Receipt Template
-# https://developers.facebook.com/docs/messenger-platform/send-api-reference/receipt-template
-
-def doReceiptTemplate(recipient_id, recipient_name, order_number, currency, payment_method, order_url, timestamp, elements, address, summary, adjustments):
-	data = json.dumps({
-	"recipient":{
-		"id":recipient_id
-	},
-	"message":{
-		"attachment":{
-			"type":"template",
-			"payload":{
-				"template_type":"receipt",
-				"recipient_name":recipient_name,
-				"order_number":order_number,
-				"currency":currency,
-				"payment_method":payment_method,		
-				"order_url":order_url,
-				"timestamp":timestamp, 
-				"elements":elements,
-				"address":address,
-				"summary":summary,
-				"adjustments":adjustments
-				}
-			}
-		}
-	})
-
-	postData(data)
-
- # simple wrapper for logging to stdout on heroku
-def log(message): 
-	print str(message)
-	sys.stdout.flush()
+def greeting(recipient_id):
+    text = "Welcome to Nova shop, what are you looking for today?"
+    buttons = [
+                  {
+                    "type":"postback",
+                    "title":"T-Shirt",
+                    "payload":"T_SHIRT"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Jean",
+                    "payload":"JEAN"
+                  },
+                  {
+                    "type":"postback",
+                    "title":"Wallet",
+                    "payload":"WALLET"
+                  }
+            ]
+    messenger.do_button_template(recipient_id, text, buttons)
 
 
 if __name__ == '__main__':
-	app.run(debug=True)
+    app.run(debug=True)
---
+++ messenger_api.py
@@ -0,0 +1,211 @@
+import json
+import requests
+import log
+
+
+class MessengerApi:
+    def __init__(self, params, headers):
+        self.params = params
+        self.headers = headers
+
+    def post_data(self, data):
+        r = requests.post("https://graph.facebook.com/v2.6/me/messages", params=self.params, headers=self.headers, data=data)
+        if r.status_code != 200:
+            log.log(r.status_code)
+            log.log(r.text)
+
+    # Sender Actions
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/sender-actions
+    def do_sender_actions(self, recipient_id):
+        data = json.dumps({
+          "recipient":{
+              "id":recipient_id
+          },
+          "sender_action":"typing_on"
+        })
+        self.post_data(data)
+
+    # Text Message
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/text-message
+    def do_text_message(self, recipient_id, message_text):
+        data = json.dumps({
+            "recipient": {
+                "id": recipient_id
+            },
+            "message": {
+                "text": message_text
+            }
+        })
+        self.post_data(data)
+
+    # Image Attachment
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/image-attachment
+    def do_image_attachment(self, recipient_id, image_url):
+        data = json.dumps({
+                "recipient":{
+                    "id":recipient_id
+                },
+                "message":{
+                    "attachment":{
+                    "type":"image",
+                    "payload":{
+                        "url":image_url
+                    }
+                }
+            }
+        })
+        self.post_data(data)
+
+    # Audio Attachment
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/audio-attachment
+    def do_audio_attachment(self, recipient_id, audio_url):
+        data = json.dumps({
+                "recipient":{
+                    "id":recipient_id
+                },
+                "message":{
+                    "attachment":{
+                    "type":"audio",
+                    "payload":{
+                        "url":audio_url
+                    }
+                }
+            }
+        })
+        self.post_data(data)
+
+    # Video Attachment
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/video-attachment
+    def do_video_attachment(self, recipient_id, video_url):
+        data = json.dumps({
+                "recipient":{
+                    "id":recipient_id
+                },
+                "message":{
+                    "attachment":{
+                    "type":"video",
+                    "payload":{
+                        "url":video_url
+                    }
+                }
+            }
+        })
+        self.post_data(data)
+
+    # File Attachment
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/file-attachment
+    def do_file_attachment(self, recipient_id, file_url):
+        data = json.dumps({
+                "recipient":{
+                    "id":recipient_id
+                },
+                "message":{
+                    "attachment":{
+                    "type":"file",
+                    "payload":{
+                        "url":file_url
+                    }
+                }
+            }
+        })
+        self.post_data(data)
+
+    # Generic Template
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/generic-template
+    def do_generic_template(self, recipient_id, elements):
+        data = json.dumps({
+            "recipient":{
+                "id":recipient_id
+            },
+            "message":{
+                "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"generic",
+                    "elements":elements
+                    }
+                }
+            }
+        })
+
+        self.post_data(data)
+
+    # Button Template
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/button-template
+    # Params:
+    #    text: must be UTF-8 and has a 320 character limit
+    #    buttons: is limited to 3
+
+    def do_button_template(self, recipient_id, text, buttons):
+        data = json.dumps({
+              "recipient":{
+                "id":recipient_id
+              },
+              "message":{
+                "attachment":{
+                  "type":"template",
+                  "payload":{
+                    "template_type":"button",
+                    "text":text,
+                    "buttons": buttons
+                  }
+                }
+              }
+            })
+
+        self.post_data(data)
+
+    # Receipt Template
+    # https://developers.facebook.com/docs/messenger-platform/send-api-reference/receipt-template
+
+    def do_receipt_template(self, recipient_id, recipient_name, order_number, currency, payment_method, order_url, timestamp, elements, address, summary, adjustments):
+        data = json.dumps({
+        "recipient":{
+            "id":recipient_id
+        },
+        "message":{
+            "attachment":{
+                "type":"template",
+                "payload":{
+                    "template_type":"receipt",
+                    "recipient_name":recipient_name,
+                    "order_number":order_number,
+                    "currency":currency,
+                    "payment_method":payment_method,
+                    "order_url":order_url,
+                    "timestamp":timestamp,
+                    "elements":elements,
+                    "address":address,
+                    "summary":summary,
+                    "adjustments":adjustments
+                    }
+                }
+            }
+        })
+
+        self.post_data(data)
+
+
+class PostbackButton:
+    def __init__(self, title, postback_value):
+        self.title = title
+        self.postback_value = postback_value
+
+    def to_json(self):
+        return json.dump({
+            "type":"postback",
+            "title":self.title,
+            "payload":self.postback_value
+        })
+
+class CallButton:
+    def __init__(self, title, phone_number):
+        self.title = title
+        self.phone_number = phone_number
+
+    def to_json(self):
+        return json.dump({
+            "type":"phone_number",
+            "title":self.title,
+            "payload":self.phone_number
+        })
